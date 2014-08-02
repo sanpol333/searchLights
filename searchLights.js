4d968c3b3a6396054197daebd49c6321e8edbcd5
sanpol333 <sanpol@inbox.ru>
2014-08-02 23:34:17 +0400

    Update: realized init

--- searchLights.js
+++ searchLights.js
@@ -1,75 +1,114 @@
-alert(1);
-
 /**
-* Подсветка текста
-* 
-* @param mixed inpSearch (строка, которую ищем)
-* @param mixed title (текст который будем подсвечивать)
-*/
-function searchLight(inpSearch,text){
-var inpSearch_clear = inpSearch.replace(/\.|\#|\`|\~|\!|\@|\$|\%|\^|\&|\*|\'|\"|\,|\<|\>|\||\;|\?|\/|\\|\)|\(/g, ' ');
-inpSearch_clear = inpSearch_clear.replace(/^\s+|\s+$/g, '');
-inpSearch_clear = inpSearch_clear.replace(/\s+/g, ' ');
-//получаем массив слов, разделенных пробелами
-var inpSearch_arr = inpSearch_clear.split(' ');  
-//сортируем массив
-for (var v=0;v<inpSearch_arr.length;v++){
-    var inpSearch_arr_current = inpSearch_arr[v];
-    for (var h=0;h<inpSearch_arr.length;h++){
-        var inpSearch_arr_reg = new RegExp(inpSearch_arr[h]);
-        var inpSearch_arr_match =inpSearch_arr_current.match(inpSearch_arr_reg);
-        if (inpSearch_arr_current !== inpSearch_arr[h] && inpSearch_arr_match !== null){
-            var inp_temp = inpSearch_arr[h];
-            inpSearch_arr[h] = inpSearch_arr_current;
-            inpSearch_arr[v] = inp_temp;
+ * @license sanpol333
+ * (c) Inc. https://github.com/sanpol333
+ * License: MIT
+ */
+var searchLights = (function(window, document) {'use strict';
+    var app = {};
+    var defaulParamValues = {
+        color: "#00CE08",
+        selectType: "text"
+    }
+    // #######################
+    // #### FUNCTION LIBS ####
+    // #######################
+     
+    // TRIM LEFT AND RIGHT SPACES
+    var trim = (function() {
+      if (!String.prototype.trim) {
+        return function(value) {
+          return (typeof value === 'string') ? value.replace(/^\s\s*/, '').replace(/\s\s*$/, '') : value;
+        };
+      }
+      return function(value) {
+        return (typeof value === 'string') ? value.trim() : value;
+      };
+    })();
+
+
+    // #######################
+    // ###### VALIDATES ######
+    // #######################
+    
+    // Validate interface
+    function validateFuncList(){
+        return {
+            selectType: validateSelectType,
+            color: validateColor
         }
     }
-}
-//переделываем слово в регулярку
-var inpSearch_arr_count = inpSearch_arr.length;
-if (inpSearch_arr[0]){
-    var reg_inpSearch_mass= Array;
-    for (var j=0;j<inpSearch_arr_count;j++){
-        var inpSearch_sp = inpSearch_arr[j];
-        var reg_inpSearch = '';
-        var inpSearch_sp_count = inpSearch_sp.length;
-        var inpSearch_sp_ch =inpSearch_sp.match(/[0-9]/);
-        if (inpSearch_sp.length <3 || inpSearch_sp_ch){
-            reg_inpSearch = inpSearch_sp;
+
+    // return type of select
+    function validateSelectType(val){
+        var isOk  = /(^text$)|(^background$)|(^underline$)/i.test(trim(val));
+        return isOk ? trim(val) : defaulParamValues.selectType;
+    }
+    // return color
+    function validateColor(val){
+        var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(trim(val));
+        return isOk ? trim(val) : defaulParamValues.color;
+    }
+
+    // validate params and return validated attributes array
+    function validateParams(params , validateFuncList, callback){
+        var paramsArr = [];
+        if( Object.prototype.toString.call(params) !== '[object Object]' ){
+            return true;
         }
-        else{
-            for(var p=0;p<inpSearch_sp_count;p++){
-                reg_inpSearch += inpSearch_sp.charAt(p)+'( )*';
+
+        for (var name in params){
+            if (params.hasOwnProperty(name) && validateFuncList[name]){
+                paramsArr.push(name);
             }
         }
-        var reg_find = new RegExp(reg_inpSearch,"ig");
-        reg_inpSearch_mass[j] = reg_find;
-        //очищаем title от тегов
-        var title_clear = text.replace(/\<span style=\"color:\#FD990D\"\>|<\/span\>/g, '');
+ 
+        callback(null,paramsArr);
+    }
 
-        //тащим строку совпавшую с регуляркой
-        var inpSearch_find = title_clear.match(reg_inpSearch_mass[j]);
-        if (inpSearch_find != null){
-            //закрашиваем найденный текст
-            var inpSearch_find_count = inpSearch_find.length;
-            for (var t=0;t<inpSearch_find_count;t++){
-                if (inpSearch_find[t] !== inpSearch_find[t-1]){
-                    var inpSearch_light = '<!'+inpSearch_find[t]+'!>';
-                    //заменяем в найденной
-                    text = text.replace(reg_inpSearch_mass[j], inpSearch_light); 
+    // #######################
+    // ######### APP #########
+    // #######################
+
+    // Constructor
+    function App(args , whiteListNameParams){
+        // write main params
+        for (var argName in validateFuncList()){
+            if (validateFuncList().hasOwnProperty(argName)){
+                // for manual options
+                if(whiteListNameParams.indexOf(argName) >= 0){
+                    var argVal = args[argName];
+                    this[argName] = validateFuncList()[argName](argVal);
+                }
+                // for auto options
+                else{
+                    this[argName] = validateFuncList()[argName]();
                 }
             }
         }
+        // РјРµС‚РѕРґС‹
+        
+
+        return this;
     }
-}
 
-//убираем повторные метки
-text = text.replace(/(\<!)+/g, '<!');
-text = text.replace(/(!\>)+/g, '!>');
+    return {
+        // initialize with parameters 
+        init : function(args) {
+            // validate arg names
+            var whiteListNameParams = [];
+            validateParams(args , validateFuncList(), function(err , data){
+                if (err) console.log(err);
+                if (data) whiteListNameParams = data;
+            });
+            // intit obj
+            if (whiteListNameParams.length > 0){
+                app = new App(args , whiteListNameParams);
+                return app;
+            }
+            else{
+                return true;
+            }
+        }
+    };
 
-//добавляем тэги
-text = text.replace(/<!/g, '<span style="color:#FD990D">');
-text = text.replace(/!>/g, '</span>'); 
-
-return text;
-}
+}(window, document));
